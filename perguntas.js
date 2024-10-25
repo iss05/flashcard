98383eb3431566698d6f610a37ffc988a8370f8c
iss05 <0000111174211xsp@al.educacao.sp.gov.br>
2024-10-25 11:03:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'O que significa quando o ph de algum líquido está no vermelho?',
+    'significa que o líquido é ácido'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quais são os três poderes?',
+    'Legislativo, Executivo e Judiciário'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são células eucariontes?',
+    'São células que não possuem núcleos'
+)
